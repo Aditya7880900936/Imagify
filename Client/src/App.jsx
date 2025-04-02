a3ebc5acd783115c357eeaa9ec914d6cf7f1efb1
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-04-03 02:39:49 +0530

    Navbar Completed and working on Hero Section

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import BuyCredit from "./Pages/BuyCredit";
+import Result from "./Pages/Result";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-5xl font-bold underline ">
-      Hello world!
-    </h1>
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy-credit" element={<BuyCredit />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
