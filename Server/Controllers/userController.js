c9a606d95c2783cebd651fc52b5f190d1ef4f922
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-04-07 18:42:28 +0530

    Login User and Register a User Api is build successfully

---
+++ Server/Controllers/userController.js
@@ -0,0 +1,82 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import userModel from "../Models/userModel.js";
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    if (!email.includes("@")) {
+      return res.status(400).json({ message: "Please enter a valid email" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const userData = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+    res.json({ success: true, user: { name: user.name }, token });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+    res.json({ success: true, user: { name: user.name }, token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const userCredits = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    const user = await userModel.findById(userId);
+    res.json({
+      success: true,
+      credits: user.creditBalance,
+      user: {
+        name: user.name,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser, loginUser };
--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import cors from 'cors';
-
+import connectDB from './Config/mongo.js';
 import 'dotenv/config';
+import userRouter from './Routes/userRoutes.js';
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json())
 app.use(cors())
+await connectDB()
+
+
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
-  res.send('Hello from Imagify server!')
+  res.send('Hello from Imagify server! Kaise hai aap log')
 })
 
 app.listen(PORT, () => {
